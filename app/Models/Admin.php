93836c166d944150e129905bebe46d52b4b14c9a
Adedaramola <paranok2017@gmail.com>
2022-07-26 11:56:00 +0100

    added factory trait to admin model

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -3,12 +3,13 @@
 namespace App\Models;
 
 use Backpack\CRUD\app\Models\Traits\CrudTrait;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Admin extends Authenticatable 
+class Admin extends Authenticatable
 {
-    use CrudTrait;
+    use CrudTrait, HasFactory;
 
     /*
     |--------------------------------------------------------------------------
