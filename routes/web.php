3ced608b7a36ec61521b4234b4894e9543668814
Adedaramola <paranok2017@gmail.com>
2022-07-26 12:06:19 +0100

    a little stuff to create dummy admin

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SiteController;
+use App\Mail\TestMail;
+use App\Models\Admin;
+use Illuminate\Support\Facades\Mail;
 
 /*
 |--------------------------------------------------------------------------
@@ -11,8 +14,18 @@ use App\Http\Controllers\SiteController;
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
-|
 */
+
+
+Route::get('/create', function () {
+    Admin::create([
+        'name' => 'Adedaramola',
+        'email' => 'mailstokindle@gmail.com',
+        'password' => bcrypt('adedaramol')
+    ]);
+
+    return "success";
+});
 
 Route::get('/', [SiteController::class, 'index']);
 
